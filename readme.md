20f3b1f97cf3c4f30d9f3d935c2542288a2f9f3a
regaliastar <1183080130@qq.com>
2019-09-26 20:18:51 +0800

    feat: wenku-copy

--- readme.md
+++ readme.md
@@ -1,1 +1,6 @@
 # awesome-plugin
+这可能是最简单的chrome小插件
+## wenku-copy
+```
+进入百度文库后，会自动在控制台打印文章所有文字
+```
--- wenku-copy/content-script.js
+++ wenku-copy/content-script.js
@@ -1,5 +1,17 @@
 ﻿// 地址：https://wenku.baidu.com/view/*
 
 (function(){
-	
+	const pageNumber = document.getElementsByClassName('reader-txt-layer').length
+	let str = ''
+	for(let i = 0; i < pageNumber; i++){
+		let coreNode = document.getElementsByClassName('reader-txt-layer')[i].childNodes[0]
+		let nodeArr = []
+		let length = coreNode.childNodes.length
+		for(let j = 0; j < length; j++){
+			nodeArr.push(coreNode.childNodes[j].innerText)
+		}
+		str += nodeArr.join('')
+	}
+
+	return str
 })()
